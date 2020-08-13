21fd29d64b86bd5c5abca84c0f0808f8d604f920
sunlin-test <1021851145@qq.com>
2020-08-13 17:37:58 +0800

    'test提交新文件'

---
+++ class_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding=utf-8
+# _*_ coding:utf-8 _*_
+print "Hello World!",
+print "Hello Again",
+print "I like typing this.",
+print "This is fun.",
+print "Yay! Printing.",
+print "I'd match rather you 'not'.",
+print 'I "said" do not match this.',
+# python2默认以ASCII编码解析，总共128位，中文为在ASCII后新增的编码，ASCII无法识别
+# 正则表达式 a+b a*b a?b +前面的一个字符a至少出现一次   *前面的一个字符a可出现任意次 ?前面的字符最多出现一次
+# . 匹配除换行符\n外的任何单字符
+# ^[abc] 不接受[]内的字符       [abc]匹配[]内的任意一个字符
+# ^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)   --两种常见写法均满足此正则
+# #sscoding:utf-8
+
+print "你好，世界！",
+print "你好，\n" \
+      "世界"
