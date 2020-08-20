ae17583f9bfd4b24c77c627b71e6b686f7995585
sunlin-test <1021851145@qq.com>
2020-08-20 15:16:57 +0800

    test pycharm commit

---
+++ class_11.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import requests
+import json
+
+
+
+def request_baidu():
+    resp = requests.get("https://baidu.com")
+    print resp.content
+
+def request_csdn():
+    resp = requests.get("https://www.csdn.net/")
+    print resp.status_code
+
+baidu_or_csdn = raw_input("if you want baidu or csdn?")
+if baidu_or_csdn == "baidu":
+    request_baidu()
+elif baidu_or_csdn == "csdn":
+    request_csdn()
+else:
+    print "现只支持baidu和csdn哦！"
+
--- class_9.py
+++ class_9.py
@@ -10,3 +10,4 @@ weight = raw_input("How much do you weight?")
 
 print "So, you're %r old,%s tall and %r heavy." % (age, height, weight)
 print test
+
