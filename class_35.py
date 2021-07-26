3cef923d75f87a21745903835ffafdb2aed5ab2b
sunlin-test <1021851145@qq.com>
2021-07-26 17:15:15 +0800

    2021/7/26-恶魔天使变装游戏

--- class_35.py
+++ class_35.py
@@ -1,17 +1,39 @@
-# coding=utf8
+# coding=utf-8
 
 """
 恶魔天使变装
 先选择角色，做出什么事情黑化值、净化值增加或减少，达到峰值，角色转换
 角色：恶魔、天使、普通人
+实习
 """
 
 blackening_value = 100
 purify_value = 100
-
+# todo 恶魔天使变装
 
 class Role:
     def __init__(self, role):
-        self.blackening_value = 0
-        self.purify_value = 100
-        # todo 恶魔天使变装
+        self.role = role
+        if role == '恶魔':
+            self.blackening_value = 100
+            self.purify_value = 0
+            self.introduce = "恶魔"
+        else:
+            self.blackening_value = 0
+            self.purify_value = 100
+            self.introduce = "天使"
+
+
+    def check_result(self):
+        if self.blackening_value >50 or self.purify_value < 50:
+            self.role = "恶魔"
+
+while True:
+    your_choose = raw_input("请选择角色> 恶魔 or 天使\n")
+    if your_choose not in ("恶魔","天使"):
+        print "错啦~ 不支持%s" % your_choose
+    else:
+        break
+
+you = Role(your_choose)
+
